0a4e19b28147cc4a76730f71cfe187f1f280fb62
Shengda <39946188+shengdamao@users.noreply.github.com>
2018-10-12 17:29:14 +0800

    Rename Recursion_Binary Tree Inorder Traversal.cpp to Recursion_Inorder_Traversal.cpp

---
+++ Recursion_Inorder_Traversal.cpp
@@ -0,0 +1,67 @@
+﻿/* 67. Binary Tree Inorder Traversal
+
+描述
+
+给出一棵二叉树,返回其中序遍历
+您在真实的面试中是否遇到过这个题？
+样例
+
+给出二叉树 {1,#,2,3},
+
+   1
+	\
+	 2
+	/
+   3
+
+返回 [1,3,2].
+挑战
+
+你能使用非递归算法来实现么?
+*/
+
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/* Definition of TreeNode:*/
+class TreeNode {
+public:
+	int val;
+	TreeNode *left, *right;
+	TreeNode(int val) {
+		this->val = val;
+		this->left = this->right = NULL;
+	}
+};
+
+class Solution {
+public:
+	/**
+	 * @param root: A Tree
+	 * @return: Inorder in ArrayList which contains node values.
+	 */
+	vector<int> inorderTraversal(TreeNode * root) {
+		vector<int> result;
+		if (root == NULL)
+			return result;
+
+		vector<int> left = inorderTraversal(root->left);
+		result.insert(result.end(), left.begin(), left.end());
+
+		result.push_back(root->val);
+
+		vector<int> right = inorderTraversal(root->right);
+		result.insert(result.end(), right.begin(), right.end());
+
+		return result;
+	}
+};
+
+int main()
+{
+    std::cout << "Don't be afraid of Binary Tree.\n"; 
+}
